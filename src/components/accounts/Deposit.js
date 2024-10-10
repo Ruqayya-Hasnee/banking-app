bc298133816e80d6daf117653de4535c2963a7b7
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-10-10 18:34:04 +0500

    adds deposit & withdraw

--- src/components/accounts/Deposit.js
+++ src/components/accounts/Deposit.js
@@ -1,4 +1,48 @@
+import { Link } from "react-router-dom";
+import "./deposit.css";
+
 const Deposit = () => {
-    return <h1>Deposit</h1>
-}
-export default Deposit
+  return (
+    <div className="create-deposit-wrapper">
+      <div className="deposit-form-wrapper">
+        <p className="top-of-deposit-form">Enter Transactions Details</p>
+        <form className="deposit-form">
+          <div className="deposit-input-wrapper">
+            <label>Amount:</label>
+            <input
+              type="text"
+              placeholder="Enter amount"
+              className="create-deposit-input"
+            />
+          </div>
+          <div className="deposit-input-wrapper">
+            <label>Type:</label>
+            <select className="select"  disabled>
+              <option value="">Select Type</option>
+              <option value="">Deposit</option>
+              <option value="">Withdraw</option>
+            </select>
+          </div>
+          <div className="deposit-description-wrapper">
+            <label>Description:</label>
+            <input
+              type="text"
+              placeholder="Enter descriptions"
+              className="description-input"
+            />
+          </div>
+        </form>
+        <div className="add-deposit-footer">
+          <div className="deposit-actions">
+            <Link to="">Enter</Link>
+          </div>
+          <div className="deposit-actions">
+            <Link to="/transactions">View all transactions</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Deposit;
--- src/components/accounts/Withdraw.js
+++ src/components/accounts/Withdraw.js
@@ -1,4 +1,47 @@
+import { Link } from "react-router-dom";
+import "./withdraw.css";
 const Withdraw = () => {
-    return <h1>Withdraw</h1>
-}
-export default Withdraw
+  return (
+    <div className="create-withdraw-wrapper">
+      <div className="withdraw-form-wrapper">
+        <p className="top-of-withdraw-form">Enter Transactions Details</p>
+        <form className="withdraw-form">
+          <div className="withdraw-input-wrapper">
+            <label>Amount:</label>
+            <input
+              type="text"
+              placeholder="Enter amount"
+              className="create-withdraw-input"
+            />
+          </div>
+          <div className="withdraw-input-wrapper">
+            <label>Type:</label>
+            <select className="select"  disabled>
+              <option value="">Select Type</option>
+              <option value="">Deposit</option>
+              <option value="">Current</option>
+            </select>
+          </div>
+          <div className="withdraw-description-wrapper">
+            <label>Description:</label>
+            <input
+              type="text"
+              placeholder="Enter transactions"
+              className="description-input"
+            />
+          </div>
+        </form>
+        <div className="add-withdraw-footer">
+          <div className="withdraw-actions">
+            <Link to="">Enter</Link>
+          </div>
+          <div className="withdraw-actions">
+            <Link to="/transactions">View all transactions</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Withdraw;
