ae00e8b8fe434b7d699a5938f70d32ff29c0d4dd
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-10-11 12:59:25 +0500

    added fix

--- src/components/transaction/Transaction.js
+++ src/components/transaction/Transaction.js
@@ -27,24 +27,25 @@ const Transaction = (props) => {
       description: description,
       time: new Date(),
     };
- 
+
     const accounts = JSON.parse(localStorage.getItem("accounts"));
     const selectedAccountIndex = accounts.findIndex((acc) => {
-       return acc.id == params.accountId
-    }) 
-    const selectedAccountBalance = accounts[selectedAccountIndex].amount
+      return acc.id == params.accountId;
+    });
+    if (selectedAccountIndex < 0) return alert("Account not found");
+    const selectedAccountBalance = accounts[selectedAccountIndex].amount;
     if (props.type == "DEPOSIT") {
-     accounts[selectedAccountIndex].amount =  selectedAccountBalance  + Number(amount)
+      accounts[selectedAccountIndex].amount =
+        selectedAccountBalance + Number(amount);
+    } else {
+      if (selectedAccountBalance < Number(amount)) {
+        alert("Insufficient balance");
+        return;
+      }
+      accounts[selectedAccountIndex].amount =
+        selectedAccountBalance - Number(amount);
     }
-   else {
-    if (selectedAccountBalance < Number(amount)) {
-      alert("Insufficient balance")
-      return 
-    }
-     accounts[selectedAccountIndex].amount =   selectedAccountBalance - Number(amount)
-   }
-   localStorage.setItem("accounts", JSON.stringify(accounts))
-
+    localStorage.setItem("accounts", JSON.stringify(accounts));
 
     const transactions = JSON.parse(localStorage.getItem("transactions"));
     if (transactions) {
