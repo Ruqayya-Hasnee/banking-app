07ee93bf1149623531ea17c8f18ce5985a4c01e7
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-10-11 13:16:13 +0500

    added fix to use effect in dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,12 +5,13 @@ import StatsCard from "./StatsCard";
 const Dashboard = () => {
   const [accountCount, setAccountCount] = useState(0);
   const [transactionCount, setTransactionCount] = useState(0);
+  
   useEffect(() => {
     const accounts = JSON.parse(localStorage.getItem("accounts"))
     const transactions = JSON.parse(localStorage.getItem("transactions"))
     setAccountCount(accounts?.length ?? 0)
     setTransactionCount(transactions?.length ?? 0)
-  })
+  }, [])
 
   return (
     <div className="dashboard-wrapper">
